a84b82e1e8ef0a91f59971f322f78652c485a72c
Aidan Carroll <aidancarroll64@gmail.com>
2024-09-05 19:32:10 +0100

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,10 +9,11 @@ function run() {
     const distFolder = core.getInput('dist-folder', { required: true });
 
     // 2. Upload files
-    const s3Uri = `s3://${bucket}`
-    exec.exec(`aws s3 sync ${distFolder} --region ${bucketRegion}`)
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom JavaScript Action!')
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl) //::set-output
 }
 
 run();
